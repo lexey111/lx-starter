3f55001b576f6dd3b7a6519eb743d0b74996dbd6
lexey111 <lexey111@gmail.com>
2020-11-15 15:05:07 +0200

    Actualize ui/display

--- src/app/pages/examples-related/ui/display/ui-tabs-example.tsx
+++ src/app/pages/examples-related/ui/display/ui-tabs-example.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Checkbox} from '../../../../engine/ui-components/data-entry/checkbox/checkbox-component';
 import {TabPane, Tabs} from '../../../../engine/ui-components/display/tabs/tabs-component';
 import {LipsumPara} from '../../../../engine/ui-components/examples-related/lipsum';
 import {SyntaxExampleTabs} from '../../../../engine/ui-components/examples-related/syntax-example-tabs';
@@ -17,18 +18,6 @@ const Markup = `<Tabs activeId={'page2'}>
 </Tabs>
 `;
 
-const JSX = <Tabs activeId={'page2'}>
-	<TabPane title={'Page 1'} id={'page1'}>
-		<LipsumPara paragraphs={2}/>
-	</TabPane>
-	<TabPane title={'Page 2 (active by default)'} id={'page2'}>
-		<LipsumPara paragraphs={2}/>
-	</TabPane>
-	<TabPane title={<span><IconHome/> Page 3</span>} id={'page3'}>
-		<LipsumPara paragraphs={2}/>
-	</TabPane>
-</Tabs>;
-
 const Syntax = `export type TTabPaneProps = {
 	title: string | JSX.Element
 	id: string
@@ -43,12 +32,28 @@ const Tabs: React.FC<TTabsProps> = (props: TTabsProps) => {...
 `;
 
 export const UiTabsExample: React.FC = () => {
+	const [showPage4, setShowPage4] = useState(false);
+
 	return <SyntaxExampleTabs
 		activePage={'result'}
 		markup={Markup}
 		syntax={Syntax}
 		result={<div>
-			{JSX}
+			<Tabs activeId={'page2'}>
+				<TabPane title={'Page 1'} id={'page1'}>
+					<LipsumPara paragraphs={2}/>
+				</TabPane>
+				<TabPane title={'Page 2 (active by default)'} id={'page2'}>
+					<LipsumPara paragraphs={2}/>
+				</TabPane>
+				<TabPane title={<span><IconHome/> Page 3</span>} id={'page3'}>
+					<Checkbox checked={showPage4} onChange={setShowPage4}>Show Page 4</Checkbox>
+					<LipsumPara paragraphs={2}/>
+				</TabPane>
+				{showPage4 && <TabPane title={'Page 4'} id={'page4'}>
+					<LipsumPara paragraphs={2}/>
+				</TabPane>}
+			</Tabs>
 		</div>}
 	/>;
 };
