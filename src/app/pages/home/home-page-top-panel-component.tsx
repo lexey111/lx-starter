5599ee833095d770248415fd182b7b759ba56396
lexey111 <lexey111@gmail.com>
2020-11-26 15:17:03 +0200

    New homepage image and header

--- src/app/pages/home/home-page-top-panel-component.tsx
+++ src/app/pages/home/home-page-top-panel-component.tsx
@@ -7,32 +7,46 @@ const columnClassName = 'home-top-column';
 
 export const HomePageTopPanel: React.FC = () => {
 	return <PageTopPanel className={'home-top-panel'}>
+		<div className={columnClassName} style={{padding: '16px 32px 16px 16px'}}>
+			<img src={'/images/crank_opt.png'} alt="crank"/>
+		</div>
 
 		<div className={columnClassName}>
-			<Title level={1} subTitle={'1xStarter | by Lexey111'}>
-				Hand crank handle kit
+			<Title level={1} subTitle={<>HandCrank kit <span>by Lexey111</span></>}>
+				1xStarter
 			</Title>
 			<p>
-				Just add some code.
+				Just remove some code and add some other code. Not vice versa!
 			</p>
 		</div>
 
 		<div className={columnClassName}>
 			<p>
-				Welcome here. This is <b>1xStarter Kit</b> and it's cool. It includes:
+				<b>Kit includes:</b>
 			</p>
-		</div>
-
-		<div className={columnClassName}>
 			<ul>
-				<li>React <WaitTag>17</WaitTag></li>
-				<li>Webpack <WaitTag>5</WaitTag></li>
-				<li>LESS <WaitTag>3</WaitTag></li>
-				<li>Typescript <WaitTag>4</WaitTag></li>
+				<li>Smart layouts</li>
+				<li>Data Store</li>
+				<li>Smart components</li>
+				<li>Configurable protected routing</li>
+				<li>Theming engine</li>
+				<li>Docs & Examples</li>
 			</ul>
 		</div>
 
-		<div className={'home-top-panel-bg'}></div>
+		<div className={columnClassName}>
+			<p>
+				<b>Under the hood:</b>
+			</p>
+			<ul>
+				<li>React <WaitTag type={'danger'}>17</WaitTag></li>
+				<li>Webpack <WaitTag type={'danger'}>5</WaitTag></li>
+				<li>LESS <WaitTag>3</WaitTag></li>
+				<li>Typescript <WaitTag type={'warning'}>4</WaitTag></li>
+				<li>MobX <WaitTag>6</WaitTag></li>
+				<li>Jest <WaitTag>26</WaitTag></li>
+			</ul>
+		</div>
 	</PageTopPanel>;
 };
 
